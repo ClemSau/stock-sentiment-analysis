55acd52412a8d4cb8268e457a20746d33107ec38
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-12-20 23:58:31 +0100

    Update scrapper

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,9 @@
-import requests
+import praw
+import pandas as pd
+import datetime as dt
 
-response = requests.get('https://www.reddit.com/r/investing/new.json?')
-
-print(response.content)
+reddit = praw.Reddit(client_id='AZdS_b1zE5tKow',
+                     client_secret='dLToiSb0pTCBGghFERqlmQnBBL6WdQ ',
+                     user_agent='reddit-sentiment-analysis',
+                     username='YOUR_REDDIT_USER_NAME',
+                     password='YOUR_REDDIT_LOGIN_PASSWORD')
