2d193b75f25418ee3999470f43a0c26f181d53bf
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2020-12-21 16:01:45 +0100

    Update scrapper

--- scrapper.py
+++ scrapper.py
@@ -1,9 +1,19 @@
 import praw
+from prawcore.exceptions import ResponseException
+import logging
 import pandas as pd
 import datetime as dt
 
+import settings.secret_settings as settings
+
 reddit = praw.Reddit(client_id='AZdS_b1zE5tKow',
-                     client_secret='dLToiSb0pTCBGghFERqlmQnBBL6WdQ ',
+                     client_secret='dLToiSb0pTCBGghFERqlmQnBBL6WdQ',
                      user_agent='reddit-sentiment-analysis',
-                     username='YOUR_REDDIT_USER_NAME',
-                     password='YOUR_REDDIT_LOGIN_PASSWORD')
+                     username= settings.reddit_username,
+                     password= settings.reddit_password)
+
+try:
+    for submission in reddit.subreddit('investing').hot(limit=10):
+        print(submission.title)
+except:
+    logging.error('Something went wrong during the authentication')
